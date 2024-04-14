8bf11a4f96183ef75b405c1d81f95b415120d0a1
OuBnOu7 <omar7bnouahi@gmail.com>
2024-04-15 00:54:46 +0200

    JSP

--- Escrim/src/application/Main.java
+++ Escrim/src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -8,13 +9,13 @@ import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+	        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view/view.fxml"));
+	        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+	        stage.setTitle("Hello!");
+	        stage.setScene(scene);
+	        stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
